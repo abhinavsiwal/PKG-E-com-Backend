8a1a6098137e973d88b81aa4eea18e78267b5c13
abhinavsiwal <abhinavsiwal@gmail.com>
2021-11-23 14:03:41 +0530

    Adding Basic Routes config

--- app.js
+++ app.js
@@ -2,4 +2,9 @@ const express = require('express');
 
 const app = express();
 
+// Import all the routes
+const products = require('./routes/product')
+
+app.use('/api/products',products);
+
 module.exports = app;
