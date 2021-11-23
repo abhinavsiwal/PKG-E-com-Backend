fdc9f5882a0fe1110a57e270d0943c24e6e02e94
abhinavsiwal <abhinavsiwal@gmail.com>
2021-11-23 16:56:01 +0530

    Get Product By Product ID

--- controllers/product-controllers.js
+++ controllers/product-controllers.js
@@ -13,45 +13,61 @@ const newProduct = async (req, res, next) => {
     numOfReviews,
     reviews,
   } = req.body;
-try {
+  try {
     const newProduct = new Product({
-        name,
-        price,
-        description,
-        ratings,
-        category,
-        seller,
-        stock,
-        numOfReviews,
-        reviews,
-    })
+      name,
+      price,
+      description,
+      ratings,
+      category,
+      seller,
+      stock,
+      numOfReviews,
+      reviews,
+    });
     const product = await newProduct.save();
     res.status(201).json({
-        success:true,
-        product,
-    })
-} catch (err) {
+      success: true,
+      product,
+    });
+  } catch (err) {
     console.log(err);
-    return res.status(400).json({message:"Adding Product Failed"})
-}
-
+    return res.status(400).json({ message: "Adding Product Failed" });
+  }
 };
 
 // Get All Products
-const getAllProducts = async(req,res)=>{
-    let products;
-    try{
-        products = await Product.find();
-    }catch(err){
-        console.log(err);
-        return res.status(500).json({ message: "Fetching Products Failed" });
-    }
-    res.status(200).json({
-        success:true,
-        count:products.length,
-        products:products.map((product)=>product.toObject()),
-    })
-}
+const getAllProducts = async (req, res) => {
+  let products;
+  try {
+    products = await Product.find();
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Fetching Products Failed" });
+  }
+  res.status(200).json({
+    success: true,
+    count: products.length,
+    products: products.map((product) => product.toObject()),
+  });
+};
+
+// Get Product By Product Id
+const getProductById = async (req, res) => {
+  const productId = req.params.id;
+  let product;
+  try {
+    product = await Product.findById(productId);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Fetching product failed" });
+  }
+  if(!product){
+      return res.status(500).json({message:"Could not find product for this Id"});
+  }
+  res.json(product);
+};
 
 exports.newProduct = newProduct;
-exports.getAllProducts = getAllProducts
+exports.getAllProducts = getAllProducts;
+exports.getProductById = getProductById;
--- routes/product.js
+++ routes/product.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const {newProduct, getAllProducts}= require('../controllers/product-controllers');
+const {newProduct, getAllProducts,getProductById}= require('../controllers/product-controllers');
 
 router.post('/product/new',newProduct);
 router.get('/products',getAllProducts);
+router.get('/product/:id',getProductById);
 
 module.exports = router;
